070b34cb03d5582759ea8e599a9ee56f67252f5a
declanosullivan <declanseer@gmail.com>
2021-11-03 17:25:58 +0000

    Update advice function to print last row from sheet

--- run.py
+++ run.py
@@ -20,21 +20,24 @@ def get_strenghts_data():
     Get strenghts from strenghts worksheet least recently used.
     Print for user to be reminded.
     """
-    strenghts = SHEET.worksheet("strenghts").get_all_values()
-    strenghts_row = strenghts[-1]
-    print(strenghts_row)
-    # Add in code to change date in last presented
+    strenghts = SHEET.worksheet("strenghts")
+    strenghts_row = strenghts.get_all_values()
+    print(strenghts_row[-1])
+    # Add code to update last presented data of strenght
+    # Decide which code is better for strenght and advice functions
 
-# def get_advice_data():
-#     """
-#     Get advice from advice worksheet least recently used.
-#     Print to display to user. 
-#     """
-
+def get_advice_data():
+    """
+    Get advice from advice worksheet least recently used.
+    Print to display to user. 
+    """
+    advice = SHEET.worksheet("advice").get_all_values()
+    advice_row = advice[-1]
+    print(advice_row)
+    # Add code to update last presented data of advice
 
 # def get_last_3_priorities():
 #     """
 #     Get previous days top 3 priorities.
 #     Present to user to confirm if done or not done on previous day.
 #     """
-
